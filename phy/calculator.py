fe9e9d3b48026f246befdac814ba26d2722b0a2d
Martin, Bertin(bertimartin) <bertimartin@paypal.com>
2017-08-23 18:42:02 +0530

    raw_input to input

--- phy/calculator.py
+++ phy/calculator.py
@@ -1,36 +1,36 @@
 def addition():
-	return float(raw_input("Number: ")) + float(raw_input("Next number: "))
+	return float(input("Number: ")) + float(input("Next number: "))
 
 def subtraction():
-	return float(raw_input("Number: ")) - float(raw_input("Next number: "))
+	return float(input("Number: ")) - float(input("Next number: "))
 
 def multiplication():
-	return float(raw_input("Number: ") * raw_input("Next number: "))
+	return float(input("Number: ") * input("Next number: "))
 
 def division():
-	return float(raw_input("Number: ")) / float(raw_input("Next number: "))
+	return float(input("Number: ")) / float(input("Next number: "))
 
 def modulo():	
-	return float(raw_input("Number: ")) % float(raw_input("Next number: "))
+	return float(input("Number: ")) % float(input("Next number: "))
 
 def exp_calcy():
 	try:
-		return eval(raw_input("Expression: "))
+		return eval(input("Expression: "))
 	except Exception as e:
 		return e
 	
 def calcy(a, b, c, d, e):
 	dispatch = {"add":a, "subtract":b, "multiplication":c, "division":d, "modulo":e}
 	try:
-		return dispatch[raw_input("Operation: ")]()
+		return dispatch[input("Operation: ")]()
 	except Exception as e:
 		return e
 		
 print("Hello,")
 while(True):
-	start_or_end = raw_input("start or end: ")
+	start_or_end = input("start or end: ")
 	if start_or_end.strip().lower() == "start":
-		which_calc = raw_input("calcy or exp_calcy(Exp = Expression): ")
+		which_calc = input("calcy or exp_calcy(Exp = Expression): ")
 		if which_calc.strip().lower() == "calcy":
 			print(calcy(addition, subtraction, multiplication, division, modulo))
 		elif which_calc.strip().lower() == "exp_calcy":
